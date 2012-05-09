e987451c4c07f0a76a134c4302bf92d76bd834f8
Andrew Towers <atowers@gmail.com>
2012-05-09 15:21:51 +1000

    Added backslash for windows paths, fixed globals

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
 
-fs = require('fs');
-walker = require('walker');
-et = require('elementtree');
-async = require('async');
+var fs = require('fs');
+var walker = require('walker');
+var et = require('elementtree');
+var async = require('async');
 
 
-var slashdot = function(s) { return s.replace(/\//g, '.').replace('.',''); };
+var slashdot = function(s) { return s.replace(/\/|\\/g, '.').replace('.',''); };
 var noop = function(){};
 
 var koth = function(master, dir, watch, callback) {
-    callback = callback ? callback : function(){}; 
+    callback = callback ? callback : function(){};
     var cache = { templates : [], master : null};
 
- 
+
     var reloadMaster = function(cb) {
         fs.readFile(master, function(err, data) {
-            if(err) { 
+            if(err) {
                 cb(err);
             } else {
                 cache.master = data+"";
@@ -36,19 +36,19 @@ var koth = function(master, dir, watch, callback) {
 
     var findTemplates = function(cb) {
         var templates = [];
-        files = [];
+        var files = [];
         walker(dir).
             on('file', function(f,stat){ files.push(f); }).
-            on('end', function() { 
+            on('end', function() {
                 async.forEach(files, function(file, ecb) {
                     fs.readFile(file, function(err, data) {
-                        if(err) { 
+                        if(err) {
                             ecb(err);
                         } else {
                             var path = slashdot(file.split('.')[0].split(dir)[1]);
                             data = "<html><body>"+data+"</body></html>";
                             var etree = et.parse(data);
-                            tpls = etree.findall('.//script'); 
+                            tpls = etree.findall('.//script');
                             for(var i=0; i<tpls.length; i++) {
                                 var tpl = tpls[i];
                                 var id = tpl.attrib['id'];
@@ -64,10 +64,10 @@ var koth = function(master, dir, watch, callback) {
     };
 
     var handler = function(req, res) {
-        if(watch || !cache.output) { 
+        if(watch || !cache.output) {
             //we're either watching files or first-run, generate output
             try {
-                cache.output = cache.master.replace("</body>", 
+                cache.output = cache.master.replace("</body>",
                         cache.templates.join("")+"</body>");
             } catch(e) {
                 throw e;
@@ -82,7 +82,7 @@ var koth = function(master, dir, watch, callback) {
         chokidar.watch(master).on('change', function(){reloadMaster(noop);});
 
     }
- 
+
     //entry point
     reloadMaster(function(){reloadTemplates(function(){callback(handler)});});
 
